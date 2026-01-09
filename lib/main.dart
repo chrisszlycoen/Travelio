ff7c3ced320aa021d5dd0885c58b71cbc9b356a2
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-01-09 12:40:03 +0200

    added some functionality to the splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:travelio/core/config/theme/app_theme.dart';
+import 'package:travelio/presentation/splash/page/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+      theme: AppTheme.lightTheme,
+    );
   }
 }
--- lib/presentation/splash/page/splash.dart
+++ lib/presentation/splash/page/splash.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:travelio/core/config/assets/app_images.dart';
+import 'package:travelio/core/config/theme/app_colors.dart';
+import 'package:travelio/presentation/onBoarding/pages/on_boarding_1.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        
-      ),
+      backgroundColor: AppColors.primary,
+      body: Center(child: Image.asset(AppImages.logo, width: 400, height: 400)),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) => const OnBoarding1()),
     );
   }
 }

e6a13424775e47e8dc626ba0d9444e2d0093b16e
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-01-11 21:43:28 +0200

    added onBoarding one page

--- lib/core/config/assets/app_images.dart
+++ lib/core/config/assets/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String basePath = 'assets/images/';
   static const String logo = '${basePath}logo.png';
+  static const String onBoarding1 = '${basePath}onboarding1.jpg';
 }
--- lib/presentation/onBoarding/pages/on_boarding_1.dart
+++ lib/presentation/onBoarding/pages/on_boarding_1.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:travelio/core/config/assets/app_images.dart';
 
 class OnBoarding1 extends StatelessWidget {
   const OnBoarding1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 6,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                    child: Image.asset(
+                      AppImages.onBoarding1,
+                      fit: BoxFit.cover,
+                    ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 20,
+                    right: 20,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Skip",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),  
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(text: "Life is short and the\nworld is"),
+                          TextSpan(text: "wide", style: TextStyle(color: Color(0xffFF7029)))
+                        ]
+                      )     
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+        ),
+    );
   }
 }
